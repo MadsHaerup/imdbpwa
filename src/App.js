f913d8b0591045879757501c3931464133aabe60
MadsHaerup <69457070+MadsHaerup@users.noreply.github.com>
2021-08-16 08:42:21 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,34 +1,30 @@
 import Index from "./views/Index";
-import {Router} from "@reach/router";
+import { Router } from "@reach/router";
 import SpecificMovie from "./components/specificMovie/SpecificMovie";
 import { useEffect } from "react";
 
 function App() {
+	useEffect(() => {
+		Notification.requestPermission(function (status) {
+			console.log("Permission status: ", status);
+		});
+	}, []);
+	function displayNotification() {
+		if (Notification.permission === "granted") {
+			navigator.serviceWorker.getRegistration().then(function (reg) {
+				reg.showNotification("Hello world!");
+			});
+		}
+	}
 
- 
-
-  useEffect(() => {
-    Notification.requestPermission (function(status) {
-      console.log( "Permission status: ", status )
-    })
-  }, [])
-  function displayNotification() {
-    if (Notification.permission === 'granted') {
-      navigator.serviceWorker.getRegistration()
-        .then(function(reg) {
-        reg.showNotification('Hello world!');
-      });
-    }
-  }
-
-  return (
-    <div className="App">
-      <button onClick={()=>displayNotification()}>Notify</button>
-       <Router>
-        <Index path="/" />
-        <SpecificMovie path="/:id"/>
-      </Router>
-    </div>
-  );
+	return (
+		<div className="App">
+			<button onClick={() => displayNotification()}>Notify</button>
+			<Router>
+				<Index path="/" />
+				<SpecificMovie path="/:id" />
+			</Router>
+		</div>
+	);
 }
 export default App;
