2183704129c4a6a27d1f0e8b7c4cc5f7443d65c9
MadsHaerup <69457070+MadsHaerup@users.noreply.github.com>
2021-08-16 09:00:31 +0200

    formatted

--- src/components/movie/SingleMovie.js
+++ src/components/movie/SingleMovie.js
@@ -1,18 +1,17 @@
-import React from 'react';
-import './Movie.css';
-import {Link } from "@reach/router";
+import React from "react";
+import "./Movie.css";
+import { Link } from "@reach/router";
 
-
-export default function SingleMovie({title, id, year, image}) {
-  return (
-    <Link className="singleMovie" to= {"/"+id}>
-      <div className="movie" key={id}>
-        <img className="movie__image" src={image} alt="" />
-        <div className="movie__info">
-          <p className="movie__title">{title} </p>
-          <p className="movie__year">{year} </p>
-        </div>
-      </div>
-    </Link>
-  )
+export default function SingleMovie({ title, id, year, image }) {
+	return (
+		<Link className="singleMovie" to={"/" + id}>
+			<div className="movie" key={id}>
+				<img className="movie__image" src={image} alt="" />
+				<div className="movie__info">
+					<p className="movie__title">{title} </p>
+					<p className="movie__year">{year} </p>
+				</div>
+			</div>
+		</Link>
+	);
 }
--- src/views/Index.js
+++ src/views/Index.js
@@ -1,28 +1,23 @@
-import React, { useState } from 'react';
-import Logo from '../components/movie/Logo';
-import MovieDB from '../components/movie/MovieDB';
-import SearchBar from '../components/searchbar/SearchBar';
+import React, { useState } from "react";
+import Logo from "../components/movie/Logo";
+import MovieDB from "../components/movie/MovieDB";
+import SearchBar from "../components/searchbar/SearchBar";
 
 export default function Index() {
-  const [search, setSearch] = useState(" ");
-  
-    const handleSearch = (event) => {
-      event.preventDefault();
-      setSearch(event.target.value);
-      console.log(search)
-    };
+	const [search, setSearch] = useState(" ");
 
-  return (
-    <div>
-      <Logo/>
-      <SearchBar 
-      handleSearch={handleSearch} 
-      search={search} 
-      setSearch={setSearch} />
+	const handleSearch = (event) => {
+		event.preventDefault();
+		setSearch(event.target.value);
+		console.log(search);
+	};
 
-      <MovieDB 
-      search={search} 
-      setSearch={setSearch} /> 
-    </div>
-  )
+	return (
+		<div>
+			<Logo />
+			<SearchBar handleSearch={handleSearch} search={search} setSearch={setSearch} />
+
+			<MovieDB search={search} setSearch={setSearch} />
+		</div>
+	);
 }
